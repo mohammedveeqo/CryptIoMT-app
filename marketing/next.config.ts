7fa080bdc803594e106f0cc6a7b81649dbcd2b56
mohammed <mohammed@veeqo.com>
2025-09-03 08:40:15 +0100

    Update marketing

--- marketing/next.config.ts
+++ marketing/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   /* config options here */
